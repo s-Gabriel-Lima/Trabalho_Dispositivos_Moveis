10eb5877f97d4fa40a62fca62cc1376e8dead388
Gabriel Lima <s.gabriel.lima22@gmail.com>
2023-11-02 23:34:50 -0300

    Initial commit

---
+++ app/src/main/java/trabalho_dispositivos_moveis/com/br/cadastro.kt
@@ -0,0 +1,11 @@
+package trabalho_dispositivos_moveis.com.br
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class cadastro : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_cadastro)
+    }
+}

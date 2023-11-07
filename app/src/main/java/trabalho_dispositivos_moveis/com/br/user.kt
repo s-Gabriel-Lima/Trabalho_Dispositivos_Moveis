276dd684375d9afe1f058ce82843ffc39e78380a
Z0ttin0 <phaz160903@gmail.com>
2023-11-07 20:48:45 -0300

    add: Tela de usuario

---
+++ app/src/main/java/trabalho_dispositivos_moveis/com/br/user.kt
@@ -0,0 +1,12 @@
+package trabalho_dispositivos_moveis.com.br
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class user : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_user)
+
+    }
+}

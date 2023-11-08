397a72563e36377bf3e632b5a56ff8a8634d12f4
Z0ttin0 <phaz160903@gmail.com>
2023-11-07 21:48:26 -0300

    add: tela de usuario
    Front-end
    Back-end

--- app/src/main/java/trabalho_dispositivos_moveis/com/br/user.kt
+++ app/src/main/java/trabalho_dispositivos_moveis/com/br/user.kt
@@ -1,16 +1,27 @@
 package trabalho_dispositivos_moveis.com.br
 
+import android.app.Activity
+import android.app.ActivityManager
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import trabalho_dispositivos_moveis.com.br.databinding.ActivityMainBinding
+import trabalho_dispositivos_moveis.com.br.databinding.ActivityUserBinding
 
 class user : AppCompatActivity() {
 
-
+private lateinit var binding: ActivityUserBinding
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_user)
+        binding = ActivityUserBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.logout.setOnClickListener {
+            val logout = Intent(this,Login::class.java)
+            startActivity(logout)
+        }
 
     }
 

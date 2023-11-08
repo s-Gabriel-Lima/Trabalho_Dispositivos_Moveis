e7bcc7fecdfa2797013e00de9822df9506efce7a
Z0ttin0 <phaz160903@gmail.com>
2023-11-07 21:08:27 -0300

    add: botão de logout

--- app/src/main/java/trabalho_dispositivos_moveis/com/br/user.kt
+++ app/src/main/java/trabalho_dispositivos_moveis/com/br/user.kt
@@ -4,9 +4,14 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class user : AppCompatActivity() {
+
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_user)
 
     }
+
 }

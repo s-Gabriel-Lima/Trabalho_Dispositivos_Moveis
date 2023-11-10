d412e3d523503ff072e851ec993c07bc45c900b8
Gabriel Lima <s.gabriel.lima22@gmail.com>
2023-11-09 22:51:35 -0300

    fix: organizando codigo de autenticação na classe correta
    Havia escrito codigo de autenticação na classe errada, esse comit ajusta o codigo na classe correta

--- app/src/main/java/trabalho_dispositivos_moveis/com/br/Login.kt
+++ app/src/main/java/trabalho_dispositivos_moveis/com/br/Login.kt
@@ -1,11 +1,46 @@
 package trabalho_dispositivos_moveis.com.br
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
+import android.graphics.Color
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.snackbar.Snackbar
+import com.google.firebase.auth.FirebaseAuth
+import trabalho_dispositivos_moveis.com.br.databinding.ActivityLoginBinding
 
 class Login : AppCompatActivity() {
+    private lateinit var binding: ActivityLoginBinding
+    private val auth = FirebaseAuth.getInstance()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.entrar.setOnClickListener{view ->
+            val email = binding.emailLogin.text.toString()
+            val senha = binding.senhaLogin.text.toString()
+
+            if(email.isEmpty() || senha.isEmpty()){
+                val snackbar = Snackbar.make(view, "Preencha todos os campos", Snackbar.LENGTH_SHORT)
+                snackbar.setBackgroundTint(Color.RED)
+                snackbar.show()
+            }else{
+                auth.signInWithEmailAndPassword(email,senha).addOnCompleteListener{autenticacao ->
+                    if(autenticacao.isSuccessful){
+                        navegartelaprincipal()
+                    }
+                }
+
+            }
+
+        }
+
+    }
+    private fun navegartelaprincipal(){
+        val intent = Intent(this,MainActivity::class.java) //AO INVES DE MAINACTIVITY COLOCAR A ACTIVITY DA TELA DE USUÁRIO QUE AINDA NÃO EXISTE NESSE BRANCH DO PROJETO
+        startActivity(intent)
+        finish()
+
+
     }
 }
--- app/src/main/java/trabalho_dispositivos_moveis/com/br/cadastro.kt
+++ app/src/main/java/trabalho_dispositivos_moveis/com/br/cadastro.kt
@@ -1,47 +1,11 @@
 package trabalho_dispositivos_moveis.com.br
 
-import android.content.Intent
-import android.graphics.Color
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.google.android.material.snackbar.Snackbar
-import com.google.firebase.auth.FirebaseAuth
-import trabalho_dispositivos_moveis.com.br.databinding.ActivityLoginBinding
 
 class cadastro : AppCompatActivity() {
-
-    private lateinit var binding:ActivityLoginBinding
-    private val auth = FirebaseAuth.getInstance()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityLoginBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-
-        binding.entrar.setOnClickListener{view ->
-            val email = binding.emailLogin.text.toString()
-            val senha = binding.senhaLogin.text.toString()
-
-            if(email.isEmpty() || senha.isEmpty()){
-                val snackbar = Snackbar.make(view, "Preencha todos os campos",Snackbar.LENGTH_SHORT)
-                snackbar.setBackgroundTint(Color.RED)
-                snackbar.show()
-            }else{
-                auth.signInWithEmailAndPassword(email,senha).addOnCompleteListener{autenticacao ->
-                    if(autenticacao.isSuccessful){
-                        navegartelaprincipal()
-                    }
-                }
-
-            }
-
-        }
-
-    }
-    private fun navegartelaprincipal(){
-        val intent = Intent(this,MainActivity::class.java) //AO INVES DE MAINACTIVITY COLOCAR A ACTIVITY DA TELA DE USUÁRIO QUE AINDA NÃO EXISTE NESSE BRANCH DO PROJETO
-        startActivity(intent)
-        finish()
-
-
+        setContentView(R.layout.activity_cadastro)
     }
 }
